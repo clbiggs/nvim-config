a89113910f5e588e6d1e45a8878fdfa7ea7b468e
Christopher Biggs <christopher.l.biggs@gmail.com>
2024-07-29 00:03:06 -0400

    updated git mappings

--- lua/biggs/lazy/fugitive.lua
+++ lua/biggs/lazy/fugitive.lua
@@ -28,6 +28,10 @@ return {
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+
+                vim.keymap.set("n", "<leader>c", function()
+                    vim.cmd.Git('commit')
+                end, opts)
             end,
         })
 
