d2900ec5f511fe5fd64f8394ae75c12499c8dbbd
Christopher Biggs <christopher.l.biggs@gmail.com>
2024-08-29 16:23:22 -0400

    added luadoc to treesitter

--- lua/biggs/lazy/treesitter.lua
+++ lua/biggs/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
       ensure_installed = {
-          "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+          "vimdoc", "luadoc", "vim", "javascript", "typescript", "c", "lua", "rust",
           "go", "c_sharp", "markdown", "markdown_inline", "jsdoc", "bash",
       },
 
