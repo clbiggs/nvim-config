38c6885dd0f5890b0dc4097d21dbcc13c803eba8
Christopher Biggs <christopher.l.biggs@gmail.com>
2025-03-26 11:00:48 -0400

    fixed ip

--- lua/biggs/lazy/markdown-preview.lua
+++ lua/biggs/lazy/markdown-preview.lua
@@ -5,7 +5,7 @@ return {
     build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
       vim.g.mkdp_open_to_the_world = 1
---      vim.g.mkdp_open_ip = '127.0.0.1'
+      vim.g.mkdp_open_ip = '127.0.0.1'
       vim.g.mkdp_port = 5050
 --      vim.g.mkdp_browser = ''
       vim.g.mkdp_echo_preview_url = 1
