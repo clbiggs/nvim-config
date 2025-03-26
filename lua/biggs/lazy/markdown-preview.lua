0ccd507d99213b75d73d11e34acdd88b40896489
Christopher Biggs <christopher.l.biggs@gmail.com>
2025-03-26 10:35:27 -0400

    Changed markdown port

--- lua/biggs/lazy/markdown-preview.lua
+++ lua/biggs/lazy/markdown-preview.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       vim.g.mkdp_open_to_the_world = 1
       vim.g.mkdp_open_ip = '127.0.0.1'
-      vim.g.mkdp_port = 8080
+      vim.g.mkdp_port = 8999
       vim.g.mkdp_browser = 'none'
       vim.g.mkdp_echo_preview_url = 1
       vim.keymap.set("n", "<leader>mp", vim.cmd.MarkdownPreviewToggle)
