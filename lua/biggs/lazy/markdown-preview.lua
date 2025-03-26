77b2dd2eb127dbd8c9b0b541c8a0ac8f64357cbb
Christopher Biggs <christopher.l.biggs@gmail.com>
2025-03-26 10:59:43 -0400

    updated markdown preview settings.

--- lua/biggs/lazy/markdown-preview.lua
+++ lua/biggs/lazy/markdown-preview.lua
@@ -5,9 +5,9 @@ return {
     build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
       vim.g.mkdp_open_to_the_world = 1
-      vim.g.mkdp_open_ip = '127.0.0.1'
-      vim.g.mkdp_port = 8999
-      vim.g.mkdp_browser = 'none'
+--      vim.g.mkdp_open_ip = '127.0.0.1'
+      vim.g.mkdp_port = 5050
+--      vim.g.mkdp_browser = ''
       vim.g.mkdp_echo_preview_url = 1
       vim.keymap.set("n", "<leader>mp", vim.cmd.MarkdownPreviewToggle)
     end
