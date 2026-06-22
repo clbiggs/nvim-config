a4c27dee7c2ecac25bd88452c30fcf5dd8c4c524
Christopher Biggs <christopher.l.biggs@gmail.com>
2026-06-22 14:53:46 -0400

    added csv viewer

--- lua/biggs/remap.lua
+++ lua/biggs/remap.lua
@@ -38,3 +38,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>vcv", "<cmd>CsvViewToggle display_mode=border header_lnum=1<CR>")
